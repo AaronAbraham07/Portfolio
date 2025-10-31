79c35b152fbdd6c57669da696fbcb792fe8cd235
AaronAbraham07 <aaronabraham091@gmail.com>
2025-10-31 20:27:30 +0530

    Refactor and optimize JavaScript animations and effects

--- script.js
+++ script.js
@@ -155,9 +155,5 @@ if (fadeInItems.length > 0) {
   window.addEventListener("load", revealItems);
 
 }
-const hamburger = document.querySelector('.hamburger');
-function toggleMenu() {
-  const navLinks = document.querySelector('.nav-links');
-  navLinks.classList.toggle('active');
-  hamburger.innerText = navLinks.classList.contains('active') ? '✖' : '☰';
-}
+
+
